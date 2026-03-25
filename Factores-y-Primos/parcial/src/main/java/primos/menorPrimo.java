4234f5e74b7c5c8aedd501848a5db85830acae55
Juanseproo <143037261+elmultiusos@users.noreply.github.com>
2026-03-25 11:09:09 -0500

    Add files via upload

---
+++ Factores-y-Primos/parcial/src/main/java/primos/menorPrimo.java
@@ -0,0 +1,17 @@
+package primos;
+
+/**
+ * Hello world!
+ */
+public class menorPrimo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+
+public class esPrimo{
+    int primo = 1;
+    int buscarPrimos = primoDado/1 and primoDado/primoDado
+
+}
+}
